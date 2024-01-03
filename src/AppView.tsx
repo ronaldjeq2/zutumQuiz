2ef78e7f5d3740303dc0bdfc8b594cb07ac4ce99
Ronald Estela <ronaldjeq@gmail.com>
2024-01-03 18:14:09 -0500

    feat: update AppView

---
+++ src/AppView.tsx
@@ -0,0 +1,15 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+export const AppView = () => {
+
+  return (
+    <>
+      <img src={reactLogo} className="logo react" alt="React logo" />
+      <h1>Dev-quiz</h1>
+
+    </>
+  )
+}
+
+export default AppView
