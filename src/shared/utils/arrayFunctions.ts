8528c8471b5a294b476da14e4ef1ba8cac35b903
Ronald Estela <ronaldjeq@gmail.com>
2024-01-04 09:33:07 -0500

    feat: added array functions

--- src/shared/utils/arrayFunctions.ts
+++ src/shared/utils/arrayFunctions.ts
@@ -1,4 +1,16 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export const reverseArraySinceKey = (items: number[], index: number) => {
     return items.slice(index).concat(items.slice(0, index))
 };
 
+export function arraysHaveTheSameLength<T>(...items: T[][]): boolean {
+    return items.every(item => item.length === items[0].length);
+}
+
+export const arrayItemsAreMajorToZero = (items: any[]) => {
+    return items.every(item => +item > 0);
+};
+
+export const arraysLengthsAreIqualOrMajorTo = (numberToPass: number, ...items: any[]) => {
+    return items.every(item => item.length >= numberToPass);
+};
